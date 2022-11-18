583a3302d5a3097e4886023543ef11eb0d9ff85a
Madhu Sudhan <m02sudhan@gmail.com>
2022-11-18 18:57:30 +0530

    Added : retrieve person detail using city or state

--- address_book_service.sql
+++ address_book_service.sql
@@ -11,10 +11,15 @@ create table AddressBook (
     email varchar(100) not null);
     insert into AddressBook (firstName, lastName, address, city, state, zip, phoneNumber, email)
     values ('Ullas','Kumar k','Hebbal','Bangalore','Karnataka',564131,5454874865,'uk00000000@gmail.com' ),
-    ('Nitish','Priyadarshi','MG Road','Gurgaon','Haryana',345641,18468489745,'np000000@gmail.com'),
-    ('Sayyed','Mutallib','Food street','Sagar','Madhya Pradesh',1245678945, 236546,'sm00000000000@gmail.com'),
-    ('Madhu','Sudhan S','Malavalli','Mandya','Karnataka',522315,1277777777,'mss0000000000@gmail.com');
+    ('Nitish','Priyadarshi','MG Road','Gurgaon','Haryana',345641,18468489745,'np000000@gmail.com' ),
+    ('Sayyed','Mutallib','Food street','Sagar','Madhya Pradesh',1245678945, 236546,'sm00000000000@gmail.com' ),
+    ('Madhu','Sudhan S','Malavalli','Mandya','Karnataka',522315,1277777777,'mss0000000000@gmail.com' );
 select * from AddressBook;
 update AddressBook set phoneNumber = 4512324565 WHERE firstName = 'Sayyed';
 update AddressBook set zip = 251235 WHERE firstName = 'Sayyed';
 delete from AddressBook where lastName = 'Sudhan S';
+insert into AddressBook (firstName, lastName, address, city, state, zip, phoneNumber, email)
+    values ('Shivam','Kumar','Whitefield','Mysore','Karnataka',584131,3454874865,'sk00000000@gmail.com' );
+    select * from AddressBook where city = 'Banaglore' or state = 'Karnataka'; 
+	select * from AddressBook where city = 'Sagar';
+    select * from AddressBook where state = 'Haryana';
